e989e556bbcc5cbf732850fd06b4016c584bbdb1
Dor1ma <admakin.pavel@gmail.com>
2024-07-05 20:35:55 +0300

    feat: add routes

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,12 +3,14 @@ package app
 import (
 	"fmt"
 	"github.com/Dor1ma/Time-Tracker/config"
+	"github.com/Dor1ma/Time-Tracker/internal/handlers"
+	"github.com/Dor1ma/Time-Tracker/internal/repositories"
+	"github.com/Dor1ma/Time-Tracker/internal/services"
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
 )
-
-var db *gorm.DB
 
 func Start() {
 	cfg, err := config.LoadConfig()
@@ -23,10 +25,46 @@ func Start() {
 		cfg.DbName,
 		cfg.DbPort)
 
+	var db *gorm.DB
+
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
 
 	RunMigration(dsn)
+
+	userRepository := repositories.NewUserRepository(db)
+	taskRepository := repositories.NewTaskRepositoryImpl(db)
+
+	userService := services.NewUserServiceImpl(userRepository, cfg.ExternalAPIURL)
+	taskService := services.NewTaskServiceImpl(taskRepository)
+
+	userHandler := handlers.NewUserHandler(userService)
+	taskHandler := handlers.NewTaskHandler(taskService)
+
+	router := gin.Default()
+
+	userRoutes := router.Group("/users")
+	{
+		userRoutes.POST("", userHandler.CreateUser)
+		userRoutes.GET("", userHandler.GetUsers)
+		userRoutes.PUT("/:id", userHandler.UpdateUser)
+		userRoutes.DELETE("/:id", userHandler.DeleteUser)
+	}
+
+	taskRoutes := router.Group("/tasks")
+	{
+		taskRoutes.POST("/start", taskHandler.StartTask)
+		taskRoutes.POST("/stop", taskHandler.StopTask)
+		taskRoutes.GET("/user/:user_id", taskHandler.GetUserTasks)
+	}
+
+	err = router.Run(":8080")
+	if err != nil {
+		log.Fatalf("Error in gin run function", err.Error())
+		return
+	}
+
+	log.Println("Gin server has started")
 }
