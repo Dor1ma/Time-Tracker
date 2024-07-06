508aea72fc7c140f4ade704d719e17b14598188a
Dor1ma <admakin.pavel@gmail.com>
2024-07-06 22:13:59 +0300

    fix: fix swagger

--- docs/docs.go
+++ docs/docs.go
@@ -516,7 +516,7 @@ const docTemplate = `{
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
 	Host:             "localhost:8080",
-	BasePath:         "/api/v1",
+	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Time Tracker API",
 	Description:      "This is a simple time tracker server.",
