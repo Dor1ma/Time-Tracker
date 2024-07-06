ee7a3574d6c8b9899a8c4c52b7b12a7a40052718
Dor1ma <admakin.pavel@gmail.com>
2024-07-06 14:48:26 +0300

    refactor: refactor update_user_request.go

--- internal/dto/update_user_request.go
+++ internal/dto/update_user_request.go
@@ -1,8 +1,8 @@
 package dto
 
 type UpdateUserRequest struct {
-	Surname    string `json:"surname"`
-	Name       string `json:"name"`
-	Patronymic string `json:"patronymic"`
-	Address    string `json:"address"`
+	Surname    string `json:"surname" binding:"required"`
+	Name       string `json:"name" binding:"required"`
+	Patronymic string `json:"patronymic" binding:"required"`
+	Address    string `json:"address" binding:"required"`
 }
