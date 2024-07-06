1ce8f1b4bac2e23a8e5af25f7c895f2886cbb4ae
Dor1ma <admakin.pavel@gmail.com>
2024-07-07 00:57:33 +0300

    refactor: refactor

---
+++ migrations/000003_test_users_data.up.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (passport_number, surname, name, patronymic, address)
+VALUES
+    ('1234 567890', 'Ivanov', 'Ivan', 'Ivanovich', '123 Main St'),
+    ('2345 678901', 'Petrov', 'Petr', 'Petrovich', '456 Elm St'),
+    ('3456 789012', 'Sidorov', 'Sidr', 'Sidorovich', '789 Pine St'),
+    ('4567 890123', 'Smirnov', 'Sergey', 'Sergeevich', '101 Maple St'),
+    ('5678 901234', 'Kuznetsov', 'Konstantin', 'Konstantinovich', '202 Oak St');
+
